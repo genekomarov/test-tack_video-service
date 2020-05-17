4bb0b09b58de8711c1db94537f47c3dc45ffb68d
genekomarov <gene.komarov@gmail.com>
2020-05-18 03:32:52 +0500

    add React.lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, Suspense} from 'react';
 import './App.css';
 import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
-import Films from "./components/content/Films/Films";
 import Footer from "./components/Footer/Footer";
-import Channels from "./components/content/Channels/Channels";
 import {Route, Redirect} from 'react-router-dom';
 import {connect} from "react-redux";
 import {getUserName} from "./redux/profile-reducer";
 
+const Films = React.lazy ( () => import("./components/content/Films/Films") );
+const Channels = React.lazy ( () => import("./components/content/Channels/Channels") );
 
 function App(props: any) {
 
@@ -21,8 +21,12 @@ function App(props: any) {
         <Header />
         <Nav />
 
-        <Route path={'/movies'} render={ () => <Films />}/>
-        <Route path={'/channels'} render={ () => <Channels />}/>
+        <Route path={'/movies'} render={ () => {
+            return <Suspense fallback={<div>Загрузка...</div>}><Films /></Suspense>
+        }}/>
+        <Route path={'/channels'} render={ () => {
+            return <Suspense fallback={<div>Загрузка...</div>}><Channels /></Suspense>
+        }}/>
         <Route path={'*'} render={ () => <Redirect to={'/movies'}/>}/>
 
         <Footer />
